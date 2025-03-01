54d959f092b0c796a63652dfe8c7c93c002e73a8
EraDigital25 <era81253@gmail.com>
2025-03-01 20:28:49 -0300

    Add files via upload

---
+++ links.js
@@ -0,0 +1,390 @@
+// Base de datos completa de enlaces
+export const links = [
+    {
+        title: "Libros de Enfermería",
+        url: "https://drive.google.com/drive/u/0/folders/1TrYrUmNl64KBcsCa3ldrt3oAKICjFJ1f",
+        category: "libros",
+        tags: ["medicina", "enfermería", "salud", "educación"]
+    },
+    {
+        title: "Libros de Medicina",
+        url: "https://drive.google.com/drive/u/0/folders/1z57HWrVjj53sgGwWp_qclZpK3D0em2Gj",
+        category: "libros",
+        tags: ["medicina", "salud", "educación"]
+    },
+    {
+        title: "Libros de Medicina Alternativo",
+        url: "https://drive.google.com/drive/mobile/folders/0B1JxsNRn6dXYYWlsNmJpajlyUjA",
+        category: "libros",
+        tags: ["medicina", "salud", "alternativo"]
+    },
+    {
+        title: "Libros de Química y Física",
+        url: "https://drive.google.com/drive/u/4/folders/1OCB22sTkrjDg9tWdk2zTyUr2gLORMEHk",
+        category: "libros",
+        tags: ["química", "física", "ciencia"]
+    },
+    {
+        title: "Libros Grandes Ideas de la Ciencia",
+        url: "https://drive.google.com/drive/folders/1sxtOzyXSEI48xrDbQS9yaHH4-83c2REB",
+        category: "libros",
+        tags: ["ciencia", "divulgación"]
+    },
+    {
+        title: "Libros Tesla",
+        url: "https://www.mediafire.com/folder/vw88qwe9vgo5g/TESLA_AND_FREE_ENERGY_LIBRARY",
+        category: "libros",
+        tags: ["tesla", "energía", "ciencia", "tecnología"]
+    },
+    {
+        title: "Libros de Ingeniería",
+        url: "https://drive.google.com/drive/u/0/folders/1giwTDasff8DlrqyUTgJvxaxMFbh9L6XW",
+        category: "libros",
+        tags: ["ingeniería", "tecnología"]
+    },
+    {
+        title: "Libros de Derecho",
+        url: "https://drive.google.com/drive/u/0/folders/13URMOYrkZYhnMCNmnSK63-vlmX7fmU6g",
+        category: "libros",
+        tags: ["derecho", "leyes"]
+    },
+    {
+        title: "Libros de Programación",
+        url: "https://drive.google.com/drive/folders/1hdSylyYGvshO7SLYz7y5ZbYEo67WKFB3",
+        category: "libros",
+        tags: ["programación", "tecnología", "desarrollo"]
+    },
+    {
+        title: "Libros Universitarios",
+        url: "https://drive.google.com/file/d/0B1NkI9FFqvUgalJVSm9RWWtqVm8/view",
+        category: "libros",
+        tags: ["universidad", "química", "física", "biología"]
+    },
+    {
+        title: "200 Libros UNAM",
+        url: "https://drive.google.com/file/d/0B8ZTQ_GAm3H0U2NabmhXaUkyQUU/view",
+        category: "libros",
+        tags: ["universidad", "UNAM", "educación"]
+    },
+    {
+        title: "Libros de Robert Kiyosaki",
+        url: "https://drive.google.com/embeddedfolderview?id=1mZaClUO34fq5W4eq_7ob565GVXuPpSRu",
+        category: "libros",
+        tags: ["finanzas", "desarrollo personal", "negocios"]
+    },
+    {
+        title: "600 Libros sobre Emprendimiento",
+        url: "https://drive.google.com/drive/folders/1MO5_7y0EI_p8gEDr2aa7ZrvvK7IUVvDu",
+        category: "libros",
+        tags: ["emprendimiento", "negocios"]
+    },
+    {
+        title: "Libros de Emprendimiento y Negocios",
+        url: "https://drive.google.com/drive/u/0/folders/1vTRZBjGfh3RUsUxrV8I0Slm0AeFSrlWn",
+        category: "libros",
+        tags: ["emprendimiento", "negocios", "desarrollo personal"]
+    },
+    {
+        title: "Libros de Liderazgo",
+        url: "https://mega.nz/folder/f4lFAJpD#1puWI9hF-8hyLt9ZD-MKgw",
+        category: "libros",
+        tags: ["liderazgo", "desarrollo personal"]
+    },
+    {
+        title: "100 Libros para Leer",
+        url: "https://drive.google.com/drive/folders/1kO7-zVgYnLHSCleFKrK7HmjLKXnYuQm8",
+        category: "libros",
+        tags: ["literatura", "general"]
+    },
+    {
+        title: "Harry Potter Colección Completa",
+        url: "https://drive.google.com/drive/folders/1tIcZFUQIrnJL9XeCtG4DtVXbnDL0w9El",
+        category: "libros",
+        tags: ["ficción", "fantasía"]
+    },
+    {
+        title: "El Señor de los Anillos",
+        url: "https://drive.google.com/drive/folders/0B7ovj3Exa2bddXhBQUdITWR4bnM",
+        category: "libros",
+        tags: ["ficción", "fantasía"]
+    },
+    {
+        title: "La Ladrona de Libros",
+        url: "https://drive.google.com/file/d/0B1Z49j_kZ40wQkxtWGdPTEF1VDg/view",
+        category: "libros",
+        tags: ["ficción", "novela"]
+    },
+    {
+        title: "El Principito",
+        url: "https://drive.google.com/file/d/1VXhS2htSuw8y9jKmw3ePVoU427CRw6Yd/view",
+        category: "libros",
+        tags: ["ficción", "clásico"]
+    },
+    {
+        title: "Libros censurados",
+        url: "https://drive.google.com/drive/folders/1hHsXEd_SDRK5_K82toWwRdxGwfUB_WmE",
+        category: "libros",
+        tags: ["misterio", "censura"]
+    },
+    {
+        title: "Pack Cursos Basicos",
+        url: "https://drive.google.com/drive/folders/1JiKCqWSf-UrmThaXwSPLjGecJgAhtB84",
+        category: "cursos",
+        tags: ["aprendizaje", "crecimiento", "educación"]
+    },
+    {
+        title: "Pack 300k Cursos",
+        url: "https://drive.google.com/drive/folders/1TQMU0Bp37CgnTkR6a3cbhokAlId7_kPS",
+        category: "cursos",
+        tags: ["aprendizaje", "crecimiento", "educación"]
+    },
+    {
+        title: "Pack Premium de Cursos",
+        url: "https://drive.google.com/drive/folders/1ChaOPKuYaqJbSkQr-B7Rq_cJU2EoeGWz",
+        category: "cursos",
+        tags: ["aprendizaje", "crecimiento", "educación"]
+    },
+    {
+        title: "Libros de Medicina (2)",
+        url: "https://drive.google.com/drive/mobile/folders/0B1JxsNRn6dXYYWlsNmJpajlyUjA",
+        category: "cursos",
+        tags: ["aprendizaje", "crecimiento", "educación"]
+    },
+    {
+        title: "650 Libros de Psicologia",
+        url: "https://drive.google.com/drive/folders/1MO5_7y0EI_p8gEDr2aa7ZrvvK7IUVvDu",
+        category: "libros",
+        tags: ["psicologia", "mente", "salud mental"]
+    },
+    {
+        title: "600 Libros Sobre Emprendimiento",
+        url: "https://drive.google.com/drive/folders/1MO5_7y0EI_p8gEDr2aa7ZrvvK7IUVvDu",
+        category: "libros",
+        tags: ["crecimiento personal", "emprendimientos", "negocios"]
+    },
+    {
+        title: "El Señor de Los Anillos",
+        url: "https://drive.google.com/drive/folders/0B7ovj3Exa2bddXhBQUdITWR4bnM?resourcekey=0-Gx7KZAIPqJFKPb5ncdDv9A",
+        category: "libros",
+        tags: ["entretenimiento", "fantasia"]
+    },
+    {
+        title: "Libros Metodologia de la Investigacion",
+        url: "https://drive.google.com/file/d/1NAomqezQFiaF8V05FjGUTJ39Lo5St6b-/view?usp=drivesdk",
+        category: "libros",
+        tags: ["investigacion", "ciencia", "metodos"]
+    },
+    {
+        title: "La Biblia del Seductor",
+        url: "https://drive.google.com/file/d/0B_xw3mdPRvsObXlmZ2dGZFlQRkk/view?usp=drivesdk&resourcekey=0-yw3CfFHXTYFuJV4Dj4IwAg",
+        category: "libros",
+        tags: ["seduccion", "manipulacion", "psicologia",]
+    },
+    {
+        title: "Libros de Como Cultivar Cannabis",
+        url: "https://drive.google.com/drive/folders/1sUisfCGoJrwCNQ99u40kcAk7DdJ96ByP",
+        category: "libros",
+        tags: ["THC", "drogas",]
+    },
+    {
+        title: "Libros de Ingenieria Mecanica",
+        url: "https://drive.google.com/drive/folders/1tY0PcRWAqT0liVxjvTvbsqOBselQBeMH",
+        category: "libros",
+        tags: ["ingenieria", "ciencia", "mecanica"]
+    },
+    {
+        title: "El Gran Libro de HTML, CSS3 y JS",
+        url: "https://drive.google.com/file/d/0B0Qr0QF2SQVSOEQyRlFpalNld0U/view?usp=drivesdk&resourcekey=0-XRvPMhOODvF-I0r8f8ihGg",
+        category: "libros",
+        tags: ["programacion", "web"]
+    },
+    {
+        title: "Test Psicologicos",
+        url: "https://drive.google.com/drive/folders/1llWQfsV0kWRS_50ZSGsV0es5Mu6rwoRm?usp=drive_link",
+        category: "libros",
+        tags: ["psicologia", "test", "salud mental"]
+    },
+    {
+        title: "Libros de Psicologia",
+        url: "https://drive.google.com/drive/folders/1cYBEApBhAqlzyfyvQ2eQ1RXGqDGl2G13?usp=drive_link",
+        category: "libros",
+        tags: ["psicologia", "salud mental"]
+    },
+    {
+        title: "Pruebas Psicologicas",
+        url: "https://drive.google.com/drive/folders/1yji6O5YIcYOjW1IG0fIhp4MJZgrcToys?usp=drive_link",
+        category: "libros",
+        tags: ["psicologia", "test", "salud mental"]
+    },
+    {
+        title: "200 Libros de Psicologia",
+        url: "https://drive.google.com/drive/folders/1SNPKjJS1y9wT3BmRUgduXhu1CrBo0Tw-?usp=sharing",
+        category: "libros",
+        tags: ["psicologia", "salud mental"]
+    },
+    {
+        title: "650 Libros de Psicologia",
+        url: "https://drive.google.com/drive/folders/1URcBMsG0cWrkb5s77pH5H3jpwRZDLO7G?usp=sharing",
+        category: "libros",
+        tags: ["psicologia", "salud mental"]
+    },
+    {
+        title: "Libros de Biodescodificacion",
+        url: "https://drive.google.com/drive/folders/1-hPu-MXhF4RyiNX_6xReb52u0aEdWbOu",
+        category: "libros",
+        tags: ["pseudociencias"]
+    },
+    {
+        title: "Libros de DIBUJARTE",
+        url: "https://drive.google.com/drive/folders/10XJUG1rq-4AtMxYq-Yax7fzwARhYGF3E",
+        category: "libros",
+        tags: ["arte", "clásico"]
+    },
+    {
+        title: "Libros de Programacion (2)",
+        url: "https://drive.google.com/drive/folders/1hdSylyYGvshO7SLYz7y5ZbYEo67WKFB3",
+        category: "libros",
+        tags: ["programacion"]
+    },
+    {
+        title: "Cursos Digitales 1",
+        url: "https://drive.google.com/drive/folders/1LN643BxeZVlhV0m5fgD008T-JM6LaIGf",
+        category: "cursos",
+        tags: ["aprendizaje", "crecimiento", "educación"]
+    },
+    {
+        title: "Super Pack 1,72 TB de Cursos",
+        url: "https://drive.google.com/drive/folder/1q3T5M6maab499-9eqXoD9ECIrcmtn7c",
+        category: "cursos",
+        tags: ["aprendizaje", "crecimiento", "educación"]
+    },
+    {
+        title: "Cursos Digitales 2",
+        url: "https://drive.google.com/drive/folders/1jCrsCY4t2AtqPiWbV1TOD4wlsvGnUQSz",
+        category: "cursos",
+        tags: ["aprendizaje", "crecimiento", "educación"]
+    },
+    {
+        title: "Series Varias",
+        url: "https://drive.google.com/drive/folders/18t3gKsHHpbCbg7cFlfa9qACBTPfEZlvZ",
+        category: "entretenimiento",
+        tags: ["entretenimiento"]
+    },
+    {
+        title: "Cursos digitales 3",
+        url: "https://drive.google.com/drive/folders/1f_etv77LVkmfDOr3dgF5twrxMlTVlF5B",
+        category: "cursos",
+        tags: ["aprendizaje", "crecimiento", "educación"]
+    },
+    {
+        title: "Audiolibros y PDFs",
+        url: "https://drive.google.com/drive/folders/1xQstrx_DQwc9LjibqPzOhYubtTSHRS_6",
+        category: "libros",
+        tags: ["entretenimiento", "literatura"]
+    },
+    {
+        title: "Peliculas Varias",
+        url: "https://drive.google.com/drive/folders/1OCFpg_9pTUnDN4KSl0wLyL36JBmDDpUP",
+        category: "entretenimiento",
+        tags: ["entretenimiento"]
+    },
+    {
+        title: "Biblioteca de Audiolibros",
+        url: "https://drive.google.com/drive/folders/1Oh_k0fJ9Pd-Dk9NuaYhXZ2xgcVuuFtHg",
+        category: "libros",
+        tags: ["literatura", "entretenimiento"]
+    },
+    {
+        title: "Biblioteca 5000 Libros",
+        url: "https://drive.google.com/drive/folders/1Hm2eW3ndQl0sJkkr3BGwGaUjTyKXX8v5",
+        category: "libros",
+        tags: ["literatura", "entretenimiento",]
+    },
+    {
+        title: "Curso de Carlos Muñoz",
+        url: "https://drive.google.com/drive/folders/1GbXmHznfao30W7XNmmnqa6suS_m519bB",
+        category: "cursos",
+        tags: ["aprendizaje", "crecimiento", "educacion"]
+    },
+    {
+        title: "Cursos de Marcos Razetti",
+        url: "https://drive.google.com/drive/folders/1sOEGGcnIkY5nZAjheqXZUf7hiGvB5Lbj",
+        category: "cursos",
+        tags: ["aprendizaje", "crecimiento", "educacion"]
+    },
+    {
+        title: "Cursos Digitales 4",
+        url: "https://drive.google.com/drive/folders/1DJFsqCn0coFct9SvUCJg8WYn8zUAmRAy",
+        category: "cursos",
+        tags: ["aprendizaje", "crecimiento", "educacion"]
+    },
+    {
+        title: "Juegos y Programas",
+        url: "https://drive.google.com/drive/folders/1ceE0IsWV0fPYtzHXmAJgIRWghW9IzQka",
+        category: "entretenimiento",
+        tags: ["entretenimiento"]
+    },
+    {
+        title: "Cursos Digitales 5",
+        url: "https://drive.google.com/drive/folders/1c8RAyhdHgpNJ-sFZ4tEuBNhWFafQWCjN",
+        category: "cursos",
+        tags: ["aprendizaje", "crecimiento", "educacion"]
+    },
+    {
+        title: "Cursos Criptomonedas",
+        url: "https://drive.google.com/drive/folders/1qyMwQVCN-WcppfYckbPBd672ie1aTo_d",
+        category: "cursos",
+        tags: ["aprendizaje", "crecimiento", "educacion", "criptomonedas"]
+    },
+    {
+        title: "Cursos Varios",
+        url: "https://drive.google.com/drive/folders/1nZb9GCiQLm-99Df8VGs3A4LM4LD2NtnY",
+        category: "cursos",
+        tags: ["aprendizaje", "crecimiento", "educacion"]
+    },
+    {
+        title: "Cursos Varios 2",
+        url: "https://drive.google.com/drive/folders/1PSxOzoP1WVqnifdbqIsDYF0cxMmY1knk",
+        category: "cursos",
+        tags: ["aprendizaje", "crecimiento", "educacion"]
+    },
+ 
+
+];
+
+// Funciones auxiliares de búsqueda
+
+export const getLinksByCategory = (category) => {
+    return links.filter(link => link.category === category);
+};
+
+export const getLinksByTag = (tag) => {
+    return links.filter(link => link.tags.includes(tag.toLowerCase()));
+};
+
+export const searchLinks = (searchTerm) => {
+    const term = searchTerm.toLowerCase();
+    return links.filter(link => 
+        link.title.toLowerCase().includes(term) ||
+        link.tags.some(tag => tag.toLowerCase().includes(term))
+    );
+};
+
+export const getCategories = () => {
+    return [...new Set(links.map(link => link.category))];
+};
+
+export const getAllTags = () => {
+    const allTags = links.reduce((acc, link) => [...acc, ...link.tags], []);
+    return [...new Set(allTags)];
+};
+
+// Exportar todo
+export default {
+    links,
+    getLinksByCategory,
+    getLinksByTag,
+    searchLinks,
+    getCategories,
+    getAllTags
+};
---
+++ main.js
@@ -0,0 +1,91 @@
+import { links } from './links.js';
+
+// Lista de contraseñas válidas
+const validPasswords = ['98765432', '10293847', '56473829', '01928374', '84736251', '90817263', '73648291', '19283746', '37482910', '48392017', '65748392', '92038471', '73829104', '83920174'];
+
+// Variable para almacenar la contraseña actual
+let correctPassword = validPasswords[Math.floor(Math.random() * validPasswords.length)];
+
+// Selección de elementos del DOM
+const welcomeScreen = document.getElementById('welcomeScreen');
+const mainContent = document.getElementById('mainContent');
+const passwordInput = document.getElementById('passwordInput');
+const enterButton = document.getElementById('enterButton');
+const togglePasswordButton = document.getElementById('togglePasswordButton');
+const searchBar = document.getElementById('searchBar');
+const linksContainer = document.getElementById('linksContainer');
+const categoryButtons = document.querySelectorAll('.category-btn');
+
+// Función para manejar el acceso con contraseña
+function handleEnter() {
+    const password = passwordInput.value;
+
+    if (validPasswords.includes(password)) {
+        welcomeScreen.style.display = 'none';
+        mainContent.style.display = 'block';
+        displayLinks(links); // Mostrar los enlaces
+    } else {
+        alert('Contraseña incorrecta');
+    }
+}
+// Funcion de redirigir a pagina web
+// Obtén el botón "redireccionar" por su ID
+document.getElementById("togglePasswordButton").addEventListener("click", function() {
+    // Redirigir a la URL deseada
+    window.location.href = "https://www.the-odin.com/"; // Reemplaza con la URL a la que deseas redirigir
+});
+
+
+// Función para mostrar los enlaces en pantalla
+function displayLinks(linksToDisplay) {
+    linksContainer.innerHTML = ''; // Limpiar el contenedor
+
+    linksToDisplay.forEach(link => {
+        const linkCard = document.createElement('div');
+        linkCard.classList.add('link-card');
+
+        linkCard.innerHTML = `
+            <h3 class="link-title">${link.title}</h3>
+            <a href="${link.url}" class="link-url" target="_blank">${link.url}</a>
+            <div class="tags">
+                ${link.tags.map(tag => `<span class="tag">${tag}</span>`).join('')}
+            </div>
+        `;
+        linksContainer.appendChild(linkCard);
+    });
+}
+
+// Filtro de categoría
+function filterCategory(category) {
+    const filteredLinks = links.filter(link => category === 'todos' || link.category === category);
+    displayLinks(filteredLinks);
+}
+
+// Filtro de búsqueda
+function filterLinks() {
+    const query = searchBar.value.toLowerCase();
+    const filteredLinks = links.filter(
+        link =>
+            link.title.toLowerCase().includes(query) ||
+            link.tags.some(tag => tag.toLowerCase().includes(query))
+    );
+    displayLinks(filteredLinks);
+}
+
+// Listeners para los botones y la barra de búsqueda
+enterButton.addEventListener('click', handleEnter);
+document.addEventListener('DOMContentLoaded', function() {
+    const categoryButtons = document.querySelectorAll('.category-btn');
+
+    // Listeners para los botones de categoría
+    categoryButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const category = button.getAttribute('data-category');
+            filterCategory(category);
+
+            // Actualizar la clase activa
+            categoryButtons.forEach(btn => btn.classList.remove('active'));
+            button.classList.add('active');
+        });
+    });
+});
